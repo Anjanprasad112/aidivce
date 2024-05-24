415b42f842612e8f313fb2404fc56ff4d1e3e8dd
Anjan Prasad <anjanprasad112@gmail.com>
2024-05-24 13:51:12 +0530

    hero section updated

--- src/app/components/HeroSecton/Hero.jsx
+++ src/app/components/HeroSecton/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
   <div className="card-body">
     <h5 className="card-title">Medical Bot</h5>
     {/* <h6 className="card-subtitle mb-2 text-body-secondary">Card subtitle</h6> */}
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card&apos;s content.</p>
     <Link href="https://aidvice-medical.streamlit.app/" target="_blank" className="card-link">Clear your medical queries</Link>
   </div>
 </div>
@@ -16,7 +16,7 @@ const Hero = () => {
   <div className="card-body">
     <h5 className="card-title">Financial Bot</h5>
     {/* <h6 className="card-subtitle mb-2 text-body-secondary">Card subtitle</h6> */}
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card&apos;s content.</p>
     <Link href="https://aidvice-medical.streamlit.app/" target="_blank" className="card-link">Need any financial help?</Link>
   </div>
 </div>
@@ -24,7 +24,7 @@ const Hero = () => {
   <div className="card-body">
     <h5 className="card-title">Legal Adviser</h5>
     {/* <h6 className="card-subtitle mb-2 text-body-secondary">Card subtitle</h6> */}
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card&apos;s content.</p>
     <Link href="https://aidvice-medical.streamlit.app/" target="_blank" className="card-link">Visit Our Legal Adviser</Link>
   </div>
 </div>
