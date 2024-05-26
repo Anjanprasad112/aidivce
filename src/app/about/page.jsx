f0b2c04c9c4c122a496a2310c6e3ddf4e72a384a
Anjan Prasad <anjanprasad112@gmail.com>
2024-05-26 19:08:54 +0530

    quot updated

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -5,7 +5,7 @@ const About = () => {
   return (
     <div className="container-lg">
       <br/><br/>
-        The project "AiDVISE" represents a pioneering endeavor in the field of conversational artificial intelligence (AI), aimed at revolutionizing the way users interact with AI-powered systems to access knowledge and receive expert advice across diverse domains. In response to the limitations of existing chatbot systems, AiDVISE introduces a sophisticated multimodal conversational interface designed to provide precise, contextually relevant, and actionable information to users in the fields of medicine, finance, and general inquiries.<br/><br/>
+        The project &quot;AiDVISE&quot; represents a pioneering endeavor in the field of conversational artificial intelligence (AI), aimed at revolutionizing the way users interact with AI-powered systems to access knowledge and receive expert advice across diverse domains. In response to the limitations of existing chatbot systems, AiDVISE introduces a sophisticated multimodal conversational interface designed to provide precise, contextually relevant, and actionable information to users in the fields of medicine, finance, and general inquiries.<br/><br/>
 
 At the core of AiDVISE lies an integration of advanced AI technologies, including large language models (LLMs), natural language processing (NLP) algorithms, and domain-specific databases. By harnessing the power of these technologies, AiDVISE offers users an immersive and intuitive experience, enabling seamless communication with the system and facilitating the retrieval of domain-specific knowledge with unparalleled accuracy and efficiency.<br/><br/>
 
